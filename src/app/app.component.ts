bb1cf7611d814844e2dc45382e071856372d61a5
Jaideep <jaideepskandari@gmail.com>
2017-07-15 15:09:08 +0530

    implemented basics of angular with typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Indian super-heroes';
+  newhero = {
+    name: '',
+  };
+  heroes = HEROES;
+  showDebugDialog = true;
+  deleteConfirm = true;
+  selectedHero = null;
+
+  addNewHero = function(name) {
+    if (name != '') {
+      console.log('new hero name: '+ name);
+      this.heroes.push ({id : this.heroes.length+1, name:name});
+      this.newhero.name = ''
+    }
+  };
+
+  removeHeroItem = function(i) {
+    var cnf = (this.deleteConfirm)? confirm('Are you sure to delete "'+ this.heroes[i].name + '"?') : true;
+    if (cnf) {
+      this.heroes.splice(i,1);
+    }
+  };
+
+  selectHero = function(hero) {
+    console.log('selecting this hero: ', hero);
+    hero = (this.selectedHero == hero)? null : hero;
+    this.selectedHero = (hero != undefined)? hero : null;
+  }
+
+  toggleDebug = function() {
+    this.showDebugDialog = (this.showDebugDialog)? false: true;
+  };
 }
+
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
