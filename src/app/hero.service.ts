f2bdde095cb0c230bc7c65e2122411b407c110df
Jaideep <jaideepskandari@gmail.com>
2017-07-15 16:05:08 +0530

    Created a Service, Mock datasource & a Promise way to get data & injected in app.component

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService{
+  getHeroes(): Promise<Hero[]> {
+    // Directly return a promise.
+    // return Promise.resolve(HEROES);
+    // Simulating a slow connection.
+    return new Promise(resolve => {
+      setTimeout(() => resolve(HEROES), 5000)
+    });
+  }
+}
